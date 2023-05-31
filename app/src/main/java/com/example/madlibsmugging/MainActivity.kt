b81cd088a68aa043b435a27a1f17b6e2aae71a10
201004354 <201004354@lbschools.net>
2023-05-31 13:49:30 -0700

    A lot of things

--- app/src/main/java/com/example/madlibsmugging/MainActivity.kt
+++ app/src/main/java/com/example/madlibsmugging/MainActivity.kt
@@ -4,8 +4,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    private EditText etName;
+
+    private EditText etCity;
+
+    private EditText etObject;
+
+    private EditText etVerb;
+
+    private EditText etObject2;
+
+    private EditText etName2;
+
+    private EditText etAdjective;
+
+    private EditText etEmotion;
+
+    private EditText etVehicle;
+
+    private Button btnGenerate;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.et_name)
+
     }
 }
