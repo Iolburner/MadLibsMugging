174cb14b2d8136de421d46f030eee99112ba148d
201004354 <201004354@lbschools.net>
2023-06-02 15:33:05 -0700

    BIG BIG progress

--- app/src/main/java/com/example/madlibsmugging/SecondActivity.java
+++ app/src/main/java/com/example/madlibsmugging/SecondActivity.java
@@ -2,13 +2,28 @@ package com.example.madlibsmugging;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private TextView tvStory;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        tvStory = findViewById(R.id.tv_story);
+
+        Intent intent = getIntent();
+
+        String story = intent.getStringExtra("story");
+
+        tvStory.setText(story);
+
+
     }
 }
